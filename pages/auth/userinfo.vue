73f8084b4f09cbf457ec01efecfec100df6ea423
sugars <sugars@gmail.com>
2020-03-15 11:17:09 +0900

    #4

---
+++ pages/auth/userinfo.vue
@@ -0,0 +1,13 @@
+<template>
+  <UserSetting />
+</template>
+
+<script>
+import { mapState, mapGetters, mapActions } from 'vuex'
+import UserSetting from '~/components/user/UserSetting'
+export default {
+  components: {
+    UserSetting,
+  },
+}
+</script>
